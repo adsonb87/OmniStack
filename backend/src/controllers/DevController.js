d0326c53501ea176ff70633f9b9aede775d937fe
adson.souza <adson.souza@aric.net>
2020-01-21 13:24:40 -0300

    Aula 2

---
+++ backend/src/controllers/DevController.js
@@ -0,0 +1,46 @@
+const axios = require('axios');
+const Dev = require('../models/Dev.js');
+
+//Index, Show, Store, Update, Destroy
+
+module.exports = {
+    async index(req,res){
+        const devs = await Dev.find();
+
+        return res.json(devs);
+    },
+
+    async store(req, res){
+        //Vai recuperar os dados do usuario do git hub
+        const { github_username, techs, latitude, longitude } = req.body;
+        
+        let dev = await Dev.findOne({ github_username });
+
+        if(!dev){
+            //Acessa a api do Github
+            const apiRes = await axios.get(`https://api.github.com/users/${github_username}`);
+        
+            //Recebe os dados da api, se oo nome for vazio ele pega o valor do login
+            const {name = login, avatar_url, bio} = apiRes.data;
+        
+            //Percorre o array de techs para retirar as virgulas e os espaços
+            const techsArray = techs.split(',').map(tech => tech.trim());
+            
+            const location = {
+                type: 'Point',
+                coordinates: [longitude, latitude],
+            };
+            //Cra o OBJ dev
+            const dev = await Dev.create({
+                github_username,
+                name,
+                avatar_url,
+                bio,
+                techs: techsArray,
+                location,
+            });
+        }      
+    
+        return res.json(dev);
+    }
+};
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,40 +1,10 @@
 const { Router } = require('express');
-const axios = require('axios');
-const Dev = require('./models/Dev.js');
+const DevController = require('./controllers/DevController.js');
 
 const routes = Router();
 
-routes.post('/devs', async (req, res) => {
-    //Vai recuperar os dados do usuario do git hub
-    const { github_username, techs, latitude, longitude } = req.body;
-
-    //Acessa a api do Github
-    const apiRes = await axios.get(`https://api.github.com/users/${github_username}`);
-
-    //Recebe os dados da api, se oo nome for vazio ele pega o valor do login
-    const {name = login, avatar_url, bio} = apiRes.data;
-
-    //Percorre o array de techs para retirar as virgulas e os espaços
-    const techsArray = techs.split(',').map(tech => tech.trim());
-    
-    const location = {
-        type: 'Point',
-        coordinates: [longitude, latitude],
-    };
-
-
-    //Cra o OBJ dev
-    const dev = await Dev.create({
-        github_username,
-        name,
-        avatar_url,
-        bio,
-        techs: techsArray,
-        location,
-    });
-
-    return res.json(dev);
-});
+routes.get('/devs', DevController.index);
+routes.post('/devs', DevController.store);
 
 
 module.exports = routes;
